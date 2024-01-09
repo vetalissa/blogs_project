7377a532b19a39697d644c4867a06af415cf0940
Elizaveta Dostovalova <lizakot20@gmail.com>
2024-01-09 19:07:07 +0300

    create post and posts view templates

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
-from posts.views import PostCreateView, PostListView, PostView
+from posts.views import (PostCreateView, PostListView, PostUserListView,
+                         PostView)
 
 app_name = 'posts'
 
 urlpatterns = [
     path('create/', PostCreateView.as_view(), name='create'),
+
     path('posts/', PostListView.as_view(), name='posts'),
-    path('paginate/<int:page>/', PostListView.as_view(), name='paginate'),
+    path('page/<int:page>/', PostListView.as_view(), name='paginate'),
+
+    path('posts_user/<int:pk>/', PostUserListView.as_view(), name='post_user'),
+    path('page/<int:page>/', PostUserListView.as_view(), name='paginate'),
+
     path('post/<int:pk>', PostView.as_view(), name='post'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -7,6 +7,7 @@ from django.views.generic.list import ListView
 from common.views import TitleMixin
 from posts.forms import PostCreateForm
 from posts.models import Post
+from users.models import User
 
 
 class HomeView(TitleMixin, TemplateView):
@@ -26,13 +27,28 @@ class PostCreateView(TitleMixin, CreateView):
         return super(PostCreateView, self).form_valid(form)
 
 
-class PostListView(TitleMixin, ListView):
+class PostMixin(ListView):
     model = Post
     queryset = Post.objects.all()
-    title = 'Все посты'
     template_name = 'posts/posts.html'
     ordering = ('-date_create')
     paginate_by = 3
+
+
+class PostListView(TitleMixin, PostMixin):
+    title = 'Все посты'
+
+
+class PostUserListView(PostMixin):
+    def get_queryset(self):
+        queryset = super(PostUserListView, self).get_queryset()
+        return queryset.filter(user=self.kwargs['pk'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostUserListView, self).get_context_data(**kwargs)
+        context['tile_username'] = User.objects.filter(pk=self.kwargs['pk']).get().username
+        context['title'] = f'Блог {User.objects.filter(pk=self.kwargs["pk"]).get().username}'
+        return context
 
 
 class PostView(TitleMixin, DetailView):
