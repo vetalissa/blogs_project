c0fcdf324a3dfef2ae00beddc478aaa2c86954c1
Elizaveta Dostovalova <lizakot20@gmail.com>
2024-06-01 17:33:59 +0300

    change PostView now PostDetailView, create title for PostListView

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from posts.views import (PostCreateView, PostListView, PostUpdateView, PostView, post_deleted, )
+from posts.views import (PostCreateView, PostDetailView, PostListView,
+                         PostUpdateView, post_deleted)
 
 app_name = 'posts'
 
@@ -8,12 +9,8 @@ urlpatterns = [
     path('create/', PostCreateView.as_view(), name='create'),
     path('update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
     path('posts', PostListView.as_view(), name='posts'),
-    path('page/<int:page>/', PostListView.as_view(), name='paginator'),
-    # path('posts_user/<int:pk>/', PostUserListView.as_view(), name='post_user'),
-    # path('page/<int:page>/', PostUserListView.as_view(), name='paginator'),
     path('posts_user/<int:pk>/', PostListView.as_view(), name='post_user'),
     path('page/<int:page>/', PostListView.as_view(), name='paginator'),
-    path('post/<int:pk>/', PostView.as_view(), name='post'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post'),
     path('deleted/<int:user_id>/<int:post_id>/', post_deleted, name='post_deleted'),
-
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
@@ -35,7 +36,7 @@ class PostCreateView(TitleMixin, CreateView):
         return super(PostCreateView, self).form_valid(form)
 
 
-class PostListView(TitleMixin, ListView):
+class PostListView(ListView):
     model = Post
     queryset = Post.objects.all()
     template_name = 'posts/posts.html'
@@ -47,13 +48,21 @@ class PostListView(TitleMixin, ListView):
         author_id = self.kwargs.get('pk')
         return queryset.filter(author_id=author_id) if author_id else queryset
 
+    def get_context_data(self, **kwargs):
+        context = super(PostListView, self).get_context_data()
+        author_id = self.kwargs['pk'] if self.kwargs else ''
+        author_page = User.objects.get(id=author_id) if author_id else ''
+        context['author_page'] = author_page
+        context['title'] = f' Посты {author_page.username}' if author_page else 'Все посты'
+        return context
 
-class PostView(TitleMixin, DetailView):
+
+class PostDetailView(TitleMixin, DetailView):
     model = Post
     template_name = 'posts/post.html'
 
     def get_context_data(self, **kwargs):
-        context = super(PostView, self).get_context_data(**kwargs)
+        context = super(PostDetailView, self).get_context_data(**kwargs)
         context['title'] = self.object.title_name
         context['comment'] = comments(self.request, self.object)
         return context
@@ -70,10 +79,10 @@ class PostUpdateView(UpdateView):
     def get_context_data(self, **kwargs):
         context = super(PostUpdateView, self).get_context_data()
         context['title'] = self.object.title_name
-        context['post'] = self.object
         return context
 
 
+@login_required
 def post_deleted(request, user_id, post_id):
     post = Post.objects.get(id=post_id)
     post.delete()
