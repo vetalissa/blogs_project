5f1c1e3f4aef34cdc64683c3ab1532aa25e92a8c
Elizaveta Dostovalova <lizakot20@gmail.com>
2024-06-02 15:05:24 +0300

    create view list for subscription

--- blog/subscriptions/urls.py
+++ blog/subscriptions/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from subscriptions.views import create_follow_sub, delete_unfollow_sub
+from subscriptions.views import create_follow_sub, delete_unfollow_sub, SubViewList, FollowViewList
 
 app_name = 'subscriptions'
 
 urlpatterns = [
     path('follow/<int:user_sub>/<int:user_follow>/', create_follow_sub, name='follow'),
     path('unfollow/<int:user_sub>/<int:user_follow>/', delete_unfollow_sub, name='unfollow'),
+    path('sub-list/<int:author_page>/', SubViewList.as_view(), name='sub_list'),
+    path('follow-list/<int:author_page>/', FollowViewList.as_view(), name='follow_list'),
 ]
--- blog/subscriptions/views.py
+++ blog/subscriptions/views.py
@@ -2,6 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import HttpResponseRedirect
 
 from subscriptions.models import Subscription
+from django.views.generic.list import ListView
+from common.views import TitleMixin
+from users.models import User
 
 
 @login_required
@@ -16,3 +19,43 @@ def delete_unfollow_sub(request, user_sub, user_follow):
     Subscription.object.unfollow_sub(user_sub, user_follow)
 
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+class SubMixinListView(ListView):
+    template_name = 'subscriptions/sub_users.html'
+    model = Subscription
+    queryset = Subscription.object.all()
+    ordering = ('-created',)
+    paginate_by = 5
+
+
+class SubViewList(SubMixinListView):
+
+    def get_queryset(self):
+        queryset = super(SubViewList, self).get_queryset()
+        author_id = self.kwargs.get('author_page')
+        return [i.user_follow for i in queryset.filter(user_sub_id=author_id)]
+
+    def get_context_data(self, **kwargs):
+        context = super(SubViewList, self).get_context_data()
+        author_page = User.objects.get(id=self.kwargs.get('author_page'))
+        context['title'] = f'Подписки у {author_page}'
+        context['non_sub'] = f'Пользователь {author_page} ни на кого не подписан'
+        context['author_page'] = author_page
+        return context
+
+
+class FollowViewList(SubMixinListView):
+
+    def get_queryset(self):
+        queryset = super(FollowViewList, self).get_queryset()
+        author_id = self.kwargs.get('author_page')
+        return [i.user_sub for i in queryset.filter(user_follow_id=author_id)]
+
+    def get_context_data(self, **kwargs):
+        context = super(FollowViewList, self).get_context_data()
+        author_page = User.objects.get(id=self.kwargs.get('author_page'))
+        context['title'] = f'Подписчики у {author_page}'
+        context['non_sub'] = f'На автора {author_page} никто не подписан...'
+        context['author_page'] = author_page
+        return context
