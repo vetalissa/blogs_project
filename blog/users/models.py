dd211c57dde6a85373f65655bb2c99cb6935bba1
Elizaveta Dostovalova <lizakot20@gmail.com>
2024-10-29 17:44:50 +0300

    create deleted old photo user avatar

--- blog/users/models.py
+++ blog/users/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_image', null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        try:
+            this = User.objects.get(id=self.id)
+            if this.image != self.image:
+                this.image.delete()
+        except:
+            pass
+        super(User, self).save(*args, **kwargs)
